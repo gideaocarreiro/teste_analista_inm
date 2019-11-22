7e760234a27471315462f0af6cb2e7425b18be21
Gideao Carreiro <gideacarreiro@gmail.com>
2019-11-22 14:13:03 -0300

    Alteracao Steps

--- features/step_definitions/api_steps.rb
+++ features/step_definitions/api_steps.rb
@@ -6,6 +6,7 @@ end
 Quando("fizer a chamada na api") do
    response = HTTParty.get("http://swapi.co/api/films/")
    puts response.body
+   puts "Titulo: " + response['results']
 end
 
 Então("validar os codigo de resposta esperado {int}") do |int|
